475a0637e9fbd4cc6b2ad3f79e0b90bdf8fa833b
AaronDC60 <aaron.de.clercq1@gmail.com>
2024-02-12 13:50:46 +0100

    Reference in for loop to improve performance

--- src_cpp/model/evidence.cpp
+++ src_cpp/model/evidence.cpp
@@ -1,54 +1,54 @@
 #include "model.h"
 
 // Count all the different observations in the dataset for a given community
-map<string, int> count_observations(vector<string> data, int community){
+map<int, int> count_observations(mcm& model, int community){
     // Trackers for the bit count
     int i;
     int j;
-    map<string, int> counts;
+    map<int, int> counts;
     // Loop over the entire dataset
-    for (string obs : data){
+    for (const vector<int>& obs : model.data){
         // Extract the substring corresponding to the community
-        string sub_obs;
+        int state = 0;
         i = 0;
         j = community;
         while(j){
             if (j & 1){
-                sub_obs += obs[i];
+                state += (obs[i] * pow(model.q, i));
             }
             i++;
             j >>= 1;
         }
         // Increase count of the substring
-        counts[sub_obs] += 1;   
+        counts[state] += 1;   
     }
     return counts;
 }
 
-double calc_evidence_icc(int community, mcm& model){
+double calc_evidence_icc(int community, mcm& model, int r){
     double log_evidence = 0;
     // Contributions from the different observations
-    map<string, int> counts = count_observations(model.data, community);
-    map<string, int>::iterator count_iter = counts.begin();
+    map<int, int> counts = count_observations(model, community);
+    map<int, int>::iterator count_iter = counts.begin();
     while (count_iter != counts.end()){
         log_evidence += (lgamma(count_iter->second + 0.5) - 0.5 * log(M_PI));
         count_iter++;
     }
     // Calculate prefactor
-    int r = counts.begin()->first.size();
     log_evidence += lgamma(pow(model.q, r)/2) - lgamma(model.N + pow(model.q, r)/2);
     return log_evidence;
 }
 
+// Input pass by reference
 double calc_evidence(vector<int> partition, mcm& model){
     double log_evidence = 0;
+    int r;
     // Iterate over all the ICCs in the partition
     for (int community : partition){
         if (community){
-            log_evidence += calc_evidence_icc(community, model);
+            r = community_size(community);
+            log_evidence += calc_evidence_icc(community, model, r);
         }
     }
     return log_evidence;
 }
-
-
