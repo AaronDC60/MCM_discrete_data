1c3d077c6be6b9e0ff07e34531ea7561f48fa343
AaronDC60 <aaron.de.clercq1@gmail.com>
2024-02-12 13:51:19 +0100

    Main file

--- src_cpp/main.cpp
+++ src_cpp/main.cpp
@@ -3,42 +3,59 @@
 
 #include <chrono>
 
+
 int main(){
     // Should be user input
-    string file = "../data/SC_voting/US_SupremeCourt_n9_N895.txt";
+    string file = "../../data/SC_voting/US_SupremeCourt_n9_N895.txt";
     int q = 2;
 
     mcm model;
-
-    // Divide and conquer scheme
     model = data_processing(file);
     model.q = q;
 
-    auto start = std::chrono::high_resolution_clock::now();
-    divide_and_conquer_setup(model);
-    cout << "Best MCM: " << endl;
-    cout << model.best_mcm[0] << endl;
-    cout << model.best_mcm[1] << endl;
-    auto stop = std::chrono::high_resolution_clock::now();
+    // Create output file
+    ofstream outputFile("output.dat");
 
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    cout << "Divide and conquer (microseconds): " << duration.count() << endl; 
+    // Exhaustive search
+    auto start = std::chrono::high_resolution_clock::now();
+    exhaustive_search(model);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+    outputFile << "Exhaustive search: " << duration.count() << "ms \n" << endl;
+
+    outputFile << "Best MCM: " << endl;
+    print_partition_to_file(outputFile, model.best_mcm);
+    outputFile << "Best log-evidence: " << model.best_evidence << "\n" << endl;
 
     // Greedy search
-    model = data_processing(file);
-    model.q = q;
-
+    model.greedy_file.open("greedy_search.dat");
     start = std::chrono::high_resolution_clock::now();
     greedy_search(model);
-    cout << "Best MCM: " << endl;
-    cout << model.best_mcm[0] << endl;
-    cout << model.best_mcm[1] << endl;
     stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    model.greedy_file.close();
+    outputFile << "Greedy search: " << duration.count() << "ms \n" << endl;    
 
-    duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    cout << "Divide and conquer (microseconds): " << duration.count() << endl; 
+    outputFile << "Best MCM: " << endl;
+    print_partition_to_file(outputFile, model.best_mcm);
+    outputFile << "Best log-evidence: " << model.best_evidence << "\n" <<endl;
 
-    cout << "Done" << endl;
+    // Divide and conquer
+    model.divide_and_conquer_file.open("divide_and_conquer.dat");
+    start = std::chrono::high_resolution_clock::now();
+    divide_and_conquer(model);
+    stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    model.divide_and_conquer_file.close();
+    outputFile << "Divide and conquer: " << duration.count() << "ms \n" << endl;    
 
+    outputFile << "Best MCM: " << endl;
+    print_partition_to_file(outputFile, model.best_mcm);
+    outputFile << "Best log-evidence: " << model.best_evidence << "\n" << endl;
+
+    outputFile << "Search done" << endl;
+    outputFile.close();
     return 0;
 }
+
